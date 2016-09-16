541ecb702725b1c3ba0298c33b4d585c2c203ec6
AxlDcc <axeldcc@gmail.com>
2016-09-16 16:26:08 -0600

    Ejercicio1
    Push,pop

---
+++ Funciones Pila/Pila.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones_Pila
+{
+    class Pila
+    {
+
+        int[] cola = new int[10];
+        int top = -1;
+        int item = 0;
+
+        public bool isEmpty()
+        {
+            if (top == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool isFull()
+        {
+            if (top == cola.Length - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int peek()
+        {
+            return cola[top];
+        }
+        public void Push()
+        {
+            while(top<9)
+            {
+                Console.Write("Igrese: ");
+                item = int.Parse(Console.ReadLine());
+                top++;
+                if (!isFull())
+                {
+                    cola[top] = item;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        public void pop()
+        {
+            if (!isEmpty())
+            {
+                cola[top] = 0;
+                top--;
+            }
+        }
+        public void Mostrar()
+        {
+            foreach (int c in cola)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+    }
+}
---
+++ Funciones Pila/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funciones_Pila
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pila lista=  new Pila();
+            short op;
+
+            do             
+            {
+                Console.WriteLine("\n Aplicacion Listas \n");
+                Console.WriteLine("1.Push");
+                Console.WriteLine("2.Pop ");
+                Console.WriteLine("3.IsFull");
+                Console.WriteLine("4.IsEmpty");
+                Console.WriteLine("5.Mostrar");
+                Console.WriteLine("6.peek");
+                Console.WriteLine("7.Salir");
+                Console.Write("Ingrese una Opcion: ");
+                op = short.Parse(Console.ReadLine());
+                switch (op)
+                
+                {
+                    case 1:
+                        lista.Push();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        lista.pop();
+                        Console.Clear();
+
+                        break;
+                    case 3:
+                        lista.isFull();
+                        Console.Clear();
+
+                        break;
+                    case 4:
+                        lista.isEmpty();
+                        Console.Clear();
+
+                        break;
+                    case 5:
+                        lista.Mostrar();
+
+                        break;
+                    case 6:
+                        lista.peek();
+                        Console.Clear();
+                        break;
+                   case 7:
+                        Console.WriteLine("Fin de la aplicacion");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcion no Valida..");
+                        break;
+                }
+
+            } while (op != 7);
+
+        }
+    }
+}
